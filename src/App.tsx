204e3b3dfaecb115486c9d0c89751034baa90f59
PearkDev <memnyyleon@gmail.com>
2024-08-01 12:16:15 +0300

    update routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,30 @@
 
-import {Outlet} from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 
 import Header from './comp/Header/Header'
+import Lots from './comp/Items/Items'
 import './style.css'
+import BuyPage from './comp/BuyPage/BuyPage';
 
+import Thanks from './comp/ThanksPage/ThanksPage';
+import Login from './comp/LoginPage/Login';
 function App() {
  
   
   //JSON.stringify(localStorage.getItem('access_token')).slice(1, -1).replace(/\W|_/g, '')
   return (
     <div className="App App--ru">
+    <Router>
         <Header/>
-        <Outlet/>
-      
+        <Routes>
+          <Route path="/" element={<Lots/>}/>
+          <Route path="/item/:id" element={<BuyPage/>}/>
+          <Route path="/thanks/" element={<Thanks/>}/>
+          <Route path="/login" element={<Login/>}/>
+        </Routes>
+        
+      </Router>
     </div>
     
   )
--- src/main.tsx
+++ src/main.tsx
@@ -1,34 +1,9 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Items from './comp/Items/Items.tsx'
-import BuyPage from './comp/BuyPage/BuyPage.tsx'
-import Thanks from './comp/ThanksPage/ThanksPage.tsx'
-import Login from './comp/LoginPage/Login.tsx'
-const router = createBrowserRouter([
-    {
-        path:"/",
-        element: <App/>,
-        children: [
-            {
-                path:'/',
-                element:<Items/>
-            },
-            {
-                path:'/item/:id',
-                element:<BuyPage/>
-            },
-            {
-                path:'/thanks/',
-                element:<Thanks/>
-            },
-            {
-                path:'/login',
-                element:<Login/>
-            },
-        ]
-    }
-])
+import { HashRouter } from 'react-router-dom'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <RouterProvider router={router}/>
+    <HashRouter>
+        <App />
+    </HashRouter>
 )
