c1933e8081677eec910ecccba0b0d6e1ff3604f6
Alyrineee <148715036+Alyrineee@users.noreply.github.com>
2024-08-01 22:44:20 +0300

    Update Login.tsx

--- src/comp/LoginPage/Login.tsx
+++ src/comp/LoginPage/Login.tsx
@@ -9,7 +9,7 @@ const Login = () => {
     if(location.search.substr(1).slice(5).length != 0)
       useEffect(()=>{
         axios.post('https://api.openspm.store/api/auth',{headers:{'code':location.search.substr(1).slice(5)}}).then((response)=>{setToken(response.data['access_token']);setRefresh(response.data['refresh_token']);location.replace('https://openspm.store/')})},[])
-    return ( <></> );
+    return ( <>{location.search.substr(1).slice(5)}</> );
 }
  
 export default Login;
