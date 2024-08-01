3e8184ff405f6de061bb2431ea396bf8d81bcfb3
Alyrineee <148715036+Alyrineee@users.noreply.github.com>
2024-08-01 23:03:11 +0300

    Update Header.tsx

--- src/comp/Header/Header.tsx
+++ src/comp/Header/Header.tsx
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import { useEffect } from "react"
 import useLocalStorage from "../../hooks/useLocalStorage"
+import axios from "axios"
 
 
 const Header = () => {
