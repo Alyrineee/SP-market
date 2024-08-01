a0849185bc7b536ac5f890bb70290bb937afdce3
PearkDev <memnyyleon@gmail.com>
2024-08-01 11:57:09 +0300

    update routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route, Outlet} from "react-router-dom";
 
 
 import Header from './comp/Header/Header'
@@ -15,17 +15,9 @@ function App() {
   //JSON.stringify(localStorage.getItem('access_token')).slice(1, -1).replace(/\W|_/g, '')
   return (
     <div className="App App--ru">
-    <Router>
         <Header/>
-        <Routes>
-          
-          <Route path="/" element={<Lots/>}/>
-          <Route path="/item/:id" element={<BuyPage/>}/>
-          <Route path="/thanks/" element={<Thanks/>}/>
-          <Route path="/login" element={<Login/>}/>
-        </Routes>
-        
-      </Router>
+        <Outlet/>
+      
     </div>
     
   )
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,34 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Items from './comp/Items/Items.tsx'
+import BuyPage from './comp/BuyPage/BuyPage.tsx'
+import Thanks from './comp/ThanksPage/ThanksPage.tsx'
+import Login from './comp/LoginPage/Login.tsx'
+const router = createBrowserRouter([
+    {
+        path:"/",
+        element: <App/>,
+        children: [
+            {
+                path:'/',
+                element:<Items/>
+            },
+            {
+                path:'/item/:id',
+                element:<BuyPage/>
+            },
+            {
+                path:'/thanks/',
+                element:<Thanks/>
+            },
+            {
+                path:'/login',
+                element:<Login/>
+            },
+        ]
+    }
+])
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <App />
+    <RouterProvider router={router}/>
 )
