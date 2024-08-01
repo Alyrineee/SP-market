088afec4ae5029c61318e7a4ccb43a2b1471fd16
Alyrineee <148715036+Alyrineee@users.noreply.github.com>
2024-08-01 23:00:09 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,14 @@ import Thanks from './comp/ThanksPage/ThanksPage';
 import Login from './comp/LoginPage/Login';
 function App() {
  
-  
+    const [_access_token, setToken] = useLocalStorage('access_token','')
+    const [_refresh_token, setRefresh] = useLocalStorage('refresh_token','')
+    
+    if(location.search.substr(1).slice(5).length != 0)
+      useEffect(()=>{
+        axios.post('https://api.openspm.store/api/auth','',
+          {headers:{'code':location.search.substr(1).slice(5)}}
+        ).then((response)=>{setToken(response.data['access_token']);setRefresh(response.data['refresh_token']);location.replace('https://openspm.store/')})},[])
   //JSON.stringify(localStorage.getItem('access_token')).slice(1, -1).replace(/\W|_/g, '')
   return (
     <div className="App App--ru">
@@ -21,7 +28,6 @@ function App() {
           <Route path="/" element={<Lots/>}/>
           <Route path="/item/:id" element={<BuyPage/>}/>
           <Route path="/thanks/" element={<Thanks/>}/>
-          <Route path="/login" element={<Login/>}/>
         </Routes>
         
       </Router>
